94b05bd422545fd0a86fe5f5e281ca1b5d84a427
Prateek <a7247434569@gmail.com>
2024-07-30 22:22:49 +0530

    DOM_project_1

---
+++ script.js
@@ -0,0 +1,10 @@
+let pro = document.getElementById('div1');
+
+// Styles:
+pro.style.backgroundColor='yellow';
+pro.style.marginL='20px';
+pro.style.padding='10px';
+pro.style.fontSize='18px';
+pro.style.fontWeight='bold';
+pro.style.height='200px';
+pro.style.width='300px';
